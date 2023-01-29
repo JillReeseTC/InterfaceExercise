29c322bf6182446ae6d2842ba8e3b1244ee0f394
Jill Reese <ajreese@gmail.com>
2023-01-29 00:12:48 -0500

    Truck and SUV classes implemented in the Program class

--- InterfaceExercise/Program.cs
+++ InterfaceExercise/Program.cs
@@ -14,11 +14,10 @@ namespace InterfaceExercise
         {
             List<IVehicle> vehicleList = new List<IVehicle>();
 
-            int carcount = 0;
-            int suvCount = 0;
-            int truckCount = 0;
-
             List<Car> cars = new List<Car>();
+            List<SUV> suvs = new List<SUV>();
+            List<Truck> trucks = new List<Truck>();
+
             
             List<ICompany> companyList = new List<ICompany>();
 
@@ -35,7 +34,6 @@ namespace InterfaceExercise
             
             vehicleList.Add(corolla);
             cars.Add(corolla);
-            carcount++;
             companyList.Add(corolla);
 
             Car prius = new Car();
@@ -49,57 +47,131 @@ namespace InterfaceExercise
 
             vehicleList.Add(prius);
             cars.Add(prius);
-            carcount++;
             companyList.Add(prius);
 
+            SUV highlander = new SUV()
+            {
+                CompanyName = "Creative Cookies",
+                HasLogo = false,
+                Make = "Toyota",
+                ModelName = "Highlander",
+                NumberOfSteeringWheels = 1,
+                NumberOfWheels = 4,
+                HasLiftDoor = true
+            };
 
+            vehicleList.Add(highlander);
+            suvs.Add(highlander);
+            companyList.Add(highlander);
 
+            SUV jeep = new SUV()
+            {
+                CompanyName = "Eco Adventures!",
+                HasLogo = true,
+                Make = "Jeep",
+                ModelName = "Grand Cherokee",
+                NumberOfSteeringWheels = 1,
+                NumberOfWheels = 4,
+                HasLiftDoor = false,
+                HornNoise = "WAAAH!"
+            };
+
+            vehicleList.Add(jeep);
+            suvs.Add(jeep);
+            companyList.Add(jeep);
+
+            Truck dodge = new Truck()
+            {
+                CompanyName = "Eco Adventures!",
+                HasLogo = true,
+                Make = "Dodge",
+                ModelName = "RAM 1500",
+                NumberOfSteeringWheels = 1,
+                NumberOfWheels = 4,
+                HasTruckBed = true,
+                HornNoise = "WHABEEP!"
+            };
+
+            vehicleList.Add(dodge);
+            trucks.Add(dodge);
+            companyList.Add(dodge);
+
+            Truck hauler = new Truck()
+            {
+                CompanyName = "Tom's Towing",
+                HasLogo = true,
+                Make = "Ford",
+                ModelName = "F450 Flatbed",
+                NumberOfSteeringWheels = 1,
+                NumberOfWheels = 4,
+                HasTruckBed = false
+            };
+
+            vehicleList.Add(hauler);
+            trucks.Add(hauler);
+            companyList.Add(hauler);
 
             //vehicles.OfType<Car>() filters for the car types. can be used in foreach to get car or other vehicle trick
             /* foreach (var item in vehicleList)
             {
                 string vehicleType = item.GetType().Name;
                 string carMake = "";
-                Console.Write($"{item.ModelName} is a {vehicleType} and has {item.NumberOfWheels} wheels and");
-                Console.WriteLine($" {item.NumberOfSteeringWheels} steering wheel.");
-               // var logoHave = (item.HasLogo) ? "does" : "does Not";
-               // carMake = item.ModelName.ToLower();
-               // Console.WriteLine($"The car belongs to {carMake.CompanyName}");
+            */
 
-                //if (vehicleType == "Car")   */
-
-                foreach (var veh in vehicleList.OfType<Car>())
-                {
-                    Console.Write($"{veh.Make} {veh.ModelName} is a {veh.GetType().Name} and has {veh.NumberOfWheels} wheels and");
-                    Console.WriteLine($" {veh.NumberOfSteeringWheels} steering wheel.");
-                    var electricHave = (veh.IsPlugInElectric) ? "does" : "does Not";
-                    Console.Write($" {electricHave} have plug-in electric,");
-                    Console.WriteLine($" and the horn noise is {veh.HornNoise}");
-                    Console.WriteLine();
-
-
-
-
-                /*
-                var myCar = item.OfType<Car>();
-                var electricHave = (myCar.IsPlugInElectric) ? "does" : "does Not";
-                Console.WriteLine($" {electricHave} have plug-in electric.");
-
-
-                //carMake = item.ModelName.ToLower();
-                //int arrayCount = 0;
-                foreach (var counter in cars)
-                {
-                    Console.Write($"{item.Make} {item.ModelName}");
-                    //var electricHave = (counter.IsPlugInElectric) ? "does" : "does Not";
-                    Console.WriteLine($" {electricHave} have plug-in electric.");
-
-                }
-                */
+            foreach (var veh in vehicleList.OfType<Car>())
+            {
+                Console.Write($"{veh.Make} {veh.ModelName} is a {veh.GetType().Name} and has {veh.NumberOfWheels} wheels and");
+                Console.WriteLine($" {veh.NumberOfSteeringWheels} steering wheel,");
+                var electricHave = (veh.IsPlugInElectric) ? "does" : "does Not";
+                Console.Write($" {electricHave} have plug-in electric,");
+                Console.WriteLine($" and the horn noise is {veh.HornNoise}");
+                Console.WriteLine();
             }
 
+            foreach (var veh in vehicleList.OfType<SUV>())
+            {
+                Console.Write($"{veh.Make} {veh.ModelName} is a {veh.GetType().Name} and has {veh.NumberOfWheels} wheels and");
+                Console.WriteLine($" {veh.NumberOfSteeringWheels} steering wheel,");
+                var liftDoor = (veh.HasLiftDoor) ? "does" : "does Not";
+                Console.Write($" {liftDoor} have a rear lift door,");
+                Console.WriteLine($" and the horn noise is {veh.HornNoise}");
+                Console.WriteLine();
+            }
 
-          //  }
+            foreach (var veh in vehicleList.OfType<Truck>())
+            {
+                Console.Write($"{veh.Make} {veh.ModelName} is a {veh.GetType().Name} and has {veh.NumberOfWheels} wheels and");
+                Console.WriteLine($" {veh.NumberOfSteeringWheels} steering wheel,");
+                var truckBed = (veh.HasTruckBed) ? "does" : "does Not";
+                Console.Write($" {truckBed} have a truck bed,");
+                Console.WriteLine($" and the horn noise is {veh.HornNoise}");
+                Console.WriteLine();
+            }
+
+            /*
+            foreach (var item in companyList)
+            {
+
+            }
+            */
+
+            /*
+            var myCar = item.OfType<Car>();
+            var electricHave = (myCar.IsPlugInElectric) ? "does" : "does Not";
+            Console.WriteLine($" {electricHave} have plug-in electric.");
+
+
+            //carMake = item.ModelName.ToLower();
+            //int arrayCount = 0;
+            foreach (var counter in cars)
+            {
+                Console.Write($"{item.Make} {item.ModelName}");
+                //var electricHave = (counter.IsPlugInElectric) ? "does" : "does Not";
+                Console.WriteLine($" {electricHave} have plug-in electric.");
+
+            }
+            */
+
 
         }
     }
